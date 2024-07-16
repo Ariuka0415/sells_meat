8cf07a933cba283554e36215c1af675f0f5e7466
riunaaMA <ariunaamagsarjav8@gmail.com>
2024-07-16 13:27:32 +0700

    added json datas

---
+++ Assets/data.js
@@ -0,0 +1,34 @@
+let data_product = [
+    {
+        id: 1,
+        name: "Үхрийн ястай мах",
+        category: "үхэр",
+        image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRx-oDBkOEi3Gmiuvone3Tuzih0oiGfry1CbOrhf9Gmc8k_c9ZA",
+        new_price: 31,
+        old_price: 39,
+    },
+    {
+        id: 2,
+        name: "Үхрийн стеакны мах",
+        category: "үхэр",
+        image: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ81zqqev5GLAatBVCFtoiK-hIfZbdUUuuRwGOzTF8Boz1Y3KDy",
+        new_price: 29,
+        old_price: "",
+    },
+    {
+        id: 3,
+        name: "Үхрийн цул мах",
+        category: "үхэр",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTXlPXBu2-gIt_Zzl-nMRuXevLMkDLK7M6LLFWEqbSCVM-4bM6",
+        new_price: 12,
+        old_price: 14,
+    },  
+    {
+        id: 4,
+        name: "Үхрийн хавтрганы мах",
+        category: "үхэр",
+        image: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ02CRSwbRG6ia0j6zMS_3yZgFIn5kB70WORDHaigsLUGEGq0tT",
+        new_price: 72,
+        old_price: '',
+    },  
+]
--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    sdsdd
   </div>
 </template>
