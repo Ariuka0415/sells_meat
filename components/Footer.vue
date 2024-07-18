b7e76ffc12edba24fb2c2a4d0516b8da5310eb72
riunaaMA <ariunaamagsarjav8@gmail.com>
2024-07-19 00:54:53 +0700

    homepage done

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,16 +1,38 @@
 <template>
-  <div class="w-full h-[60vh] bg-[#252420] mt-[100px]">
+  <div
+    class="w-full h-[70vh] bg-[#252420] mt-[100px] text-[#ffffff] flex flex-col justify-center items-center gap-[30px]"
+  >
     <img
       class="w-[200px]"
       src="file:///C:/Users/Swift%203/Downloads/Component%2011%20(1).svg"
       alt=""
     />
-    <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+    <ul class="footer-links flex list-disc gap-[40px] text-[20px]">
+      <li class="cursor-pointer hover:list-none">Company</li>
+      <li class="cursor-pointer hover:list-none">Products</li>
+      <li class="cursor-pointer hover:list-none">Offices</li>
+      <li class="cursor-pointer hover:list-none">About</li>
+      <li class="cursor-pointer hover:list-none">Contact</li>
     </ul>
+    <div class="footer-social-icons flex gap-8">
+      <img
+        src="file:///C:/Users/Swift%203/Downloads/mdi_instagram.svg"
+        alt=""
+      />
+      <img
+        src="file:///C:/Users/Swift%203/Downloads/ic_baseline-facebook.svg"
+        alt=""
+      />
+      <img
+        src="file:///C:/Users/Swift%203/Downloads/ic_outline-whatsapp.svg"
+        alt=""
+      />
+    </div>
+    <div
+      class="copyright flex flex-col items-center gap-[30px] w-full mb-[30px] text-[20px]"
+    >
+      <hr class="w-[80%] border-none h-[1px] bg-[#c7c7c7]" />
+      <p>Copyright @ 2024 - All right reserved</p>
+    </div>
   </div>
 </template>
