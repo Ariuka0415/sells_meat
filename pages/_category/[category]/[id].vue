03bfca9518c1868717bdcb2657bdbfe4f8b71305
riunaaMA <ariunaamagsarjav8@gmail.com>
2024-07-21 17:03:42 +0700

    productDisplay done

--- pages/_category/[category]/[id].vue
+++ pages/_category/[category]/[id].vue
@@ -1,12 +1,180 @@
 <template>
-  <div class="crum">
-    HOME
-    <Icon name="material-symbols:arrow-forward-ios" style="color: black" />
-    SHOP
-    <Icon name="material-symbols:arrow-forward-ios" style="color: black" />
-    {{ all_product.category }}
-    <Icon name="material-symbols:arrow-forward-ios" style="color: black" />
-    {{ all_product.name }}
+  <div class="my-10">
+    <div
+      class="crum flex items-center gap-[8px] text-[#3d4142] text-[16px] font-bold my-[30px] mx-[80px]"
+    >
+      ГЭР
+      <Icon name="material-symbols:arrow-forward-ios" style="color: black" />
+      МАХ
+      <Icon name="material-symbols:arrow-forward-ios" style="color: black" />
+      <span class="capitalize"> {{ category }}</span>
+      <Icon name="material-symbols:arrow-forward-ios" style="color: black" />
+      <span v-if="currenProduct" class="capitalize">{{
+        currenProduct.name
+      }}</span>
+    </div>
+    <div class="display flex my-0 mx-[100px]">
+      <div class="display-left gap-[15px]">
+        <div class="mainImg w-[540px] object-cover">
+          <img
+            class="h-[550px] object-cover"
+            v-if="currenProduct.category === 'үхэр'"
+            src="https://crowdcow-images.imgix.net/https%3A%2F%2Fcrowdcow-uploads.imgix.net%2Fpicture%2Fproduction%2Figg3bjkkud0%2FRibeye_Beef_Product_Grain_1.jpg?ixlib=rails-2.1.4&fit=crop&alt=Image%20of%20Boneless%20Ribeye%20Steak&data=%7B%22product-detail--image-switcher-target%22%3D%3E%22mainImage%22%7D&auto=compress%2Cformat&cs=srgb&w=720&h=720&s=1f2283bc7fccf835ae547285f320fe99"
+            alt=""
+          />
+          <img
+            v-else-if="currenProduct.category === 'гахай'"
+            class="h-[550px] object-cover"
+            src="https://crowdcow-images.imgix.net/https%3A%2F%2Fcrowdcow-uploads.imgix.net%2Fpicture%2Fproduction%2Fijhd1ezdr3k%2Fproduct_TrueStory_FullCrop_BonelessPorkChop_RawStyled_.jpg?ixlib=rails-2.1.4&fit=crop&alt=Image%20of%20Kurobuta%20Boneless%20Pork%20Chop&data=%7B%22product-detail--image-switcher-target%22%3D%3E%22mainImage%22%7D&auto=compress%2Cformat&cs=srgb&w=720&h=720&s=020da7e75a8024c3fdda901e2b5be1dd"
+            alt=""
+          />
+          <img
+            v-else-if="currenProduct.category === 'хонь'"
+            class="h-[550px] object-cover"
+            src="https://crowdcow-images.imgix.net/https%3A%2F%2Fcrowdcow-uploads.imgix.net%2Fpicture%2Fproduction%2Fidvczlfpepg%2Fculotte_w_cap_Wagyu_Cross_Product_Mishima-Edit.jpg?ixlib=rails-2.1.4&fit=crop&alt=Image%20of%20Picanha&data=%7B%22product-detail--image-switcher-target%22%3D%3E%22mainImage%22%7D&auto=compress%2Cformat&cs=srgb&w=720&h=720&s=2347987a3934eb06e8d4b441d3867029"
+            alt=""
+          />
+          <img
+            v-else-if="currenProduct.category === 'ямаа'"
+            class="h-[550px] object-cover"
+            src="https://crowdcow-images.imgix.net/https%3A%2F%2Fcrowdcow-uploads.imgix.net%2Fpicture%2Fproduction%2Fiw9kpnxhv7o%2FBone-in_ribeye_Wagyu_Cross_Product_Mishima.jpg?ixlib=rails-2.1.4&fit=crop&alt=Image%20of%20Wagyu%20Bone-in%20Ribeye%20Steak&data=%7B%22product-detail--image-switcher-target%22%3D%3E%22mainImage%22%7D&auto=compress%2Cformat&cs=srgb&w=720&h=720&s=bd81f2bc874df7e9244b09d681c2fb56"
+            alt=""
+          />
+          <img
+            v-else-if="currenProduct.category === 'тахиа'"
+            class="h-[550px] object-cover"
+            src="https://crowdcow-images.imgix.net/https%3A%2F%2Fcrowdcow-uploads.imgix.net%2Fpicture%2Fproduction%2Fie30cgsxkth%2FThighs_Chicken_Product_Pasture_Raised.jpg?ixlib=rails-2.1.4&fit=crop&alt=Image%20of%20Heirloom%20Chicken%20Thighs&data=%7B%22product-detail--image-switcher-target%22%3D%3E%22mainImage%22%7D&auto=compress%2Cformat&cs=srgb&w=720&h=720&s=ede5fe3f5ac0dbf4eed5fc7e8eda9406"
+            alt=""
+          />
+          <img
+            v-else-if="currenProduct.category === 'бусад'"
+            class="h-[550px] w-[586px] object-cover"
+            src="https://crowdcow-images.imgix.net/https%3A%2F%2Fcrowdcow-uploads.imgix.net%2Fpicture%2Fproduction%2Ficcwkm6dygp%2FSteak-Lover-Box.jpg?ixlib=rails-2.1.4&fit=crop&alt=Image%20of%20Steak%20Lover%20Box&data=%7B%22product-detail--image-switcher-target%22%3D%3E%22mainImage%22%7D&auto=compress%2Cformat&cs=srgb&w=720&h=720&s=177851ca197da45870b2fd91e107840d"
+            alt=""
+          />
+        </div>
+        <div class="img-list flex gap-[10px] my-3 justify-center items-center">
+          <img
+            class="h-[100px] w-[100px] object-cover"
+            :src="currenProduct.image"
+            alt=""
+          />
+          <img
+            class="h-[100px] w-[100px] object-cover"
+            :src="currenProduct.image"
+            alt=""
+          />
+          <img
+            class="h-[100px] w-[100px] object-cover"
+            :src="currenProduct.image"
+            alt=""
+          />
+          <img
+            class="h-[100px] w-[100px] object-cover"
+            :src="currenProduct.image"
+            alt=""
+          />
+          <img
+            class="h-[100px] w-[100px] object-cover"
+            :src="currenProduct.image"
+            alt=""
+          />
+        </div>
+      </div>
+      <div class="diplay-right flex flex-col my-0 mx-[70px]">
+        <h1 class="text-[#3d3d3d] text-[40px] font-bold">
+          {{ currenProduct.name }}
+        </h1>
+        <div
+          class="stars flex items-center mt-[13px] gap-[5px] text-[#1c1c1c] text-[16px]"
+        >
+          <Icon name="ic:sharp-star-purple500" style="color: #c53831" />
+          <Icon name="ic:sharp-star-purple500" style="color: #c53831" />
+          <Icon name="ic:sharp-star-purple500" style="color: #c53831" />
+          <Icon name="ic:sharp-star-purple500" style="color: #c53831" />
+          <Icon name="ic:sharp-star-purple500" style="color: #c53831" />
+          <p>(54)</p>
+        </div>
+        <div
+          class="prices flex my-[10px] mx-0 gap-[20px] text-[20px] font-bold"
+        >
+          <div>${{ currenProduct.new_price }}</div>
+          <div
+            class="text-[#c53831] line-through"
+            v-if="currenProduct.old_price"
+          >
+            ${{ currenProduct.old_price }}
+          </div>
+        </div>
+        <div class="description my-4">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, amet
+          fugiat praesentium impedit earum mollitia delectus quisquam neque,
+          repellat unde, minus ut sequi voluptate labore atque aperiam quibusdam
+          eligendi sint!
+        </div>
+        <h1 class="text-[#3d3d3d] my-[10px] gap-5 text-[24px] font-bold">
+          Жин: <span class="ml-14 font-medium">1кг</span>
+        </h1>
+        <h1 class="text-[#3d3d3d] my-[10px] gap-5 text-[24px] font-bold">
+          Ангилал:
+          <span class="ml-14 font-medium capitalize">{{
+            currenProduct.category
+          }}</span>
+        </h1>
+        <div class="addCart flex justify-between items-center">
+          <h6 class="text-[24px] font-bold my-[20px]">
+            Нийт үнэ: ${{ parseInt(currenProduct.new_price * value) }}
+          </h6>
+          <div class="flex items-center">
+            <button
+              @click="value--"
+              class="rounded-full w-[30px] flex items-center justify-center h-[30px] bg-[#ede9e8] mx-[10px] hover:bg-[#e1e4ea]"
+            >
+              <Icon name="ic:round-minus" style="color: black" />
+            </button>
+            <span class="text-[20px] font-semibold">{{ value }}</span>
+            <button
+              @click="value++"
+              class="rounded-full w-[30px] flex items-center justify-center h-[30px] bg-[#ede9e8] mx-[10px] hover:bg-[#e1e4ea]"
+            >
+              <Icon name="material-symbols:add-rounded" style="color: black" />
+            </button>
+          </div>
+        </div>
+        <button
+          class="text-[#ffffff] flex justify-center items-center w-[160px] mx-[5px] h-[45px] bg-[#c53831] text-[16px] font-semibold my-3 cursor-pointer hover:bg-[#862d28] hover:shadow-md transition duration-300"
+        >
+          Картанд хийх
+        </button>
+
+        <div
+          class="giftBox flex my-5 justify-start items-center text-[25px] font-semibold text-[#3d3d3d]"
+        >
+          <Icon name="flowbite:gift-box-solid" style="color: black" />
+          <h1 class="ml-2">Бэлгийн багц руу хийх</h1>
+        </div>
+      </div>
+    </div>
+    <div
+      v-if="currenProduct.category != 'бусад'"
+      class="relatedProducts h-[80vh] my-10 flex flex-col justify-center"
+    >
+      <h1 class="text-[30px] font-bold mx-[80px] my-6">
+        Холбоотой бүтээгдэхүүн
+      </h1>
+      <div class="products mt-5 mb-10 mx-[100px] grid grid-cols-4 gap-6">
+        <Item
+          v-for="(item, id) in filteredProducts"
+          :key="id"
+          :id="item.id"
+          :image="item.image"
+          :name="item.name"
+          :old_price="item.old_price"
+          :new_price="item.new_price"
+          :category="item.category"
+        />
+      </div>
+    </div>
   </div>
 </template>
 <script setup>
@@ -14,10 +182,8 @@ import { useRoute } from "vue-router";
 
 const route = useRoute();
 const category = route.params.category;
-const id = route.params.id;
-
-console.log("Category:", category);
-console.log("ID:", id);
+const id = Number(route.params.id);
+const value = ref(0);
 
 const all_product = [
   {
@@ -273,4 +439,17 @@ const all_product = [
     old_price: 100.9,
   },
 ];
+
+const currenProduct = computed(() => {
+  return all_product.find((product) => product.id === id);
+});
+console.log(currenProduct.new_price);
+console.log(value);
+// const totalPrice = computed(() => {
+//   return currenProduct.new_price.value * value.value;
+// });
+
+const filteredProducts = computed(() => {
+  return all_product.filter((item) => item.category === category);
+});
 </script>
