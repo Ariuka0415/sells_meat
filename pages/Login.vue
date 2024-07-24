e44f46f72800f503b09403df15dcbffd959c5aef
riunaaMA <ariunaamagsarjav8@gmail.com>
2024-07-24 10:55:00 +0700

    done

--- pages/Login.vue
+++ pages/Login.vue
@@ -1,26 +1,26 @@
 <template>
   <div
-    className="loginsignup w-[100%] h-[100vh] bg-[#ffffff] my-[70px] bg-opacity-85"
+    className="loginsignup w-[100%] bg-[#ffffff] my-[70px] bg-opacity-85 md:my-[20px] md:px-20 sm:px-0"
   >
     <div
-      className="loginsignup-container w-[680px] h-[600px] bg-white shadow-lg  m-auto py-[40px] px-[60px]"
+      className="loginsignup-container w-[680px] h-[600px] bg-white shadow-lg  m-auto py-[40px] px-[60px] md:w-full sm:w-full sm:px-[30px]"
     >
-      <h1 class="my-[20px] mx-[0px] text-[30px]">Нэвтрэх</h1>
-      <div className="loginsignup-fields flex flex-col gap-[29px] mt-[30px]">
+      <h1 class="my-[20px] mx-[0px] text-[30px] md:my-[15px] md:text-[25px] sm:my-[10px] sm:text-[20px]">Нэвтрэх</h1>
+      <div className="loginsignup-fields flex flex-col gap-[29px] mt-[30px] sm:gap-[20px] sm:mt-[20px]">
         <!-- <input type="text" placeholder="Your Name" /> -->
         <input
           type="email"
           placeholder="И-Мэйл хаяг"
-          class="h-[72px] w-full pl-[20px] border outline-none text-[#5c5c5c] text-[18px] rounded-xl"
+          class="h-[72px] w-full sm:h-[60px] pl-[20px] border outline-none text-[#5c5c5c] text-[18px] rounded-xl sm:text-[15px]"
         />
         <input
           type="password"
           placeholder="Нууц үг"
-          class="h-[72px] w-full pl-[20px] border outline-none text-[#5c5c5c] text-[18px] rounded-xl"
+          class="h-[72px] sm:h-[60px] sm:text-[15px] w-full pl-[20px] border outline-none text-[#5c5c5c] text-[18px] rounded-xl"
         />
       </div>
       <button
-        class="w-full rounded-xl h-[72px] text-white bg-[#ff4141] mt-[30px] border-none text-[24px] font-medium cursor-pointer hover:bg-[#c53831]"
+        class="w-full rounded-xl h-[72px] sm:h-[60px] sm:text-[20px] text-white bg-[#ff4141] mt-[30px] border-none text-[24px] font-medium cursor-pointer hover:bg-[#c53831]"
       >
         Нэвтрэх
       </button>
--- pages/Signup.vue
+++ pages/Signup.vue
@@ -1,30 +1,28 @@
 <template>
-  <div
-    className="loginsignup w-[100%] h-[105vh] bg-[#ffffff] bg-opacity-85 my-4 "
-  >
+  <div className="loginsignup w-[100%] bg-[#ffffff] bg-opacity-85 my-4 ">
     <div
-      className="loginsignup-container w-[680px] h-[600px] bg-white shadow-lg m-auto py-[40px] px-[60px]"
+      className="loginsignup-container w-[680px] h-[600px] bg-white shadow-lg m-auto py-[40px] px-[60px] sm:px-[20px] sm:w-full md:w-full"
     >
       <h1 class="font-semibold mx-[0px] text-[20px]">Бүртгүүлэх</h1>
       <div className="loginsignup-fields flex flex-col gap-[29px] mt-[30px]">
         <input
           type="text"
-          class="h-[60px] w-full pl-[20px] border outline-none text-[#5c5c5c] text-[18px] rounded-xl"
+          class="h-[60px] sm:h-[50px] sm:text-[15px] w-full pl-[20px] border outline-none text-[#5c5c5c] text-[18px] rounded-xl"
           placeholder="Your Name"
         />
         <input
           type="email"
           placeholder="И-Мэйл хаяг"
-          class="h-[60px] w-full pl-[20px] border outline-none text-[#5c5c5c] text-[18px] rounded-xl"
+          class="h-[60px] sm:h-[50px] sm:text-[15px] w-full pl-[20px] border outline-none text-[#5c5c5c] text-[18px] rounded-xl"
         />
         <input
           type="password"
           placeholder="Нууц үг"
-          class="h-[60px] w-full pl-[20px] border outline-none text-[#5c5c5c] text-[18px] rounded-xl"
+          class="h-[60px] sm:h-[50px] sm:text-[15px] w-full pl-[20px] border outline-none text-[#5c5c5c] text-[18px] rounded-xl"
         />
       </div>
       <button
-        class="w-full rounded-xl h-[60px] text-white bg-[#ff4141] mt-[30px] border-none text-[24px] font-medium cursor-pointer hover:bg-[#c53831]"
+        class="w-full rounded-xl h-[60px] sm:h-[50px] text-white bg-[#ff4141] mt-[30px] border-none text-[24px] font-medium cursor-pointer hover:bg-[#c53831]"
       >
         Бүртгүүлэх
       </button>
@@ -34,7 +32,7 @@
         Бүртгэлтэй бол ...
         <NuxtLink to="/Login">
           <span
-            class="text-[#ff4141] font-semibold hover:underline cursor-pointer hover:duration-500 "
+            class="text-[#ff4141] font-semibold hover:underline cursor-pointer hover:duration-500"
             >Нэвтрэх</span
           ></NuxtLink
         >
